d3b22960a945edf5f83a60056a50eb43bb0d5430
Saugat Acharya <mesaugat@gmail.com>
2017-10-27 19:48:54 +0545

    Update typescript declaration file

--- index.d.ts
+++ index.d.ts
@@ -1,120 +1,121 @@
 declare module 'keycode-js' {
-    enum KeyCode {
-        KEY_CANCEL,
-        KEY_HELP,
-        KEY_BACK_SPACE,
-        KEY_TAB,
-        KEY_CLEAR,
-        KEY_RETURN,
-        KEY_ENTER,
-        KEY_SHIFT,
-        KEY_CONTROL,
-        KEY_ALT,
-        KEY_PAUSE,
-        KEY_CAPS_LOCK,
-        KEY_ESCAPE,
-        KEY_SPACE,
-        KEY_PAGE_UP,
-        KEY_PAGE_DOWN,
-        KEY_END,
-        KEY_HOME,
-        KEY_LEFT,
-        KEY_UP,
-        KEY_RIGHT,
-        KEY_DOWN,
-        KEY_PRINTSCREEN,
-        KEY_INSERT,
-        KEY_DELETE,
-        KEY_0,
-        KEY_1,
-        KEY_2,
-        KEY_3,
-        KEY_4,
-        KEY_5,
-        KEY_6,
-        KEY_7,
-        KEY_8,
-        KEY_9,
-        KEY_SEMICOLON,
-        KEY_EQUALS,
-        KEY_A,
-        KEY_B,
-        KEY_C,
-        KEY_D,
-        KEY_E,
-        KEY_F,
-        KEY_G,
-        KEY_H,
-        KEY_I,
-        KEY_J,
-        KEY_K,
-        KEY_L,
-        KEY_M,
-        KEY_N,
-        KEY_O,
-        KEY_P,
-        KEY_Q,
-        KEY_R,
-        KEY_S,
-        KEY_T,
-        KEY_U,
-        KEY_V,
-        KEY_W,
-        KEY_X,
-        KEY_Y,
-        KEY_Z,
-        KEY_CONTEXT_MENU,
-        KEY_NUMPAD0,
-        KEY_NUMPAD1,
-        KEY_NUMPAD2,
-        KEY_NUMPAD3,
-        KEY_NUMPAD4,
-        KEY_NUMPAD5,
-        KEY_NUMPAD6,
-        KEY_NUMPAD7,
-        KEY_NUMPAD8,
-        KEY_NUMPAD9,
-        KEY_MULTIPLY,
-        KEY_ADD,
-        KEY_SEPARATOR,
-        KEY_SUBTRACT,
-        KEY_DECIMAL,
-        KEY_DIVIDE,
-        KEY_F1,
-        KEY_F2,
-        KEY_F3,
-        KEY_F4,
-        KEY_F5,
-        KEY_F6,
-        KEY_F7,
-        KEY_F8,
-        KEY_F9,
-        KEY_F10,
-        KEY_F11,
-        KEY_F12,
-        KEY_F13,
-        KEY_F14,
-        KEY_F15,
-        KEY_F16,
-        KEY_F17,
-        KEY_F18,
-        KEY_F19,
-        KEY_F20,
-        KEY_F21,
-        KEY_F22,
-        KEY_F23,
-        KEY_F24,
-        KEY_NUM_LOCK,
-        KEY_SCROLL_LOCK,
-        KEY_COMMA,
-        KEY_PERIOD,
-        KEY_SLASH,
-        KEY_BACK_QUOTE,
-        KEY_OPEN_BRACKET,
-        KEY_BACK_SLASH,
-        KEY_CLOSE_BRACKET,
-        KEY_QUOTE,
-        KEY_META,
+
+    namespace KeyCode {
+        export const KEY_CANCEL;
+        export const KEY_HELP;
+        export const KEY_BACK_SPACE;
+        export const KEY_TAB;
+        export const KEY_CLEAR;
+        export const KEY_RETURN;
+        export const KEY_ENTER;
+        export const KEY_SHIFT;
+        export const KEY_CONTROL;
+        export const KEY_ALT;
+        export const KEY_PAUSE;
+        export const KEY_CAPS_LOCK;
+        export const KEY_ESCAPE;
+        export const KEY_SPACE;
+        export const KEY_PAGE_UP;
+        export const KEY_PAGE_DOWN;
+        export const KEY_END;
+        export const KEY_HOME;
+        export const KEY_LEFT;
+        export const KEY_UP;
+        export const KEY_RIGHT;
+        export const KEY_DOWN;
+        export const KEY_PRINTSCREEN;
+        export const KEY_INSERT;
+        export const KEY_DELETE;
+        export const KEY_0;
+        export const KEY_1;
+        export const KEY_2;
+        export const KEY_3;
+        export const KEY_4;
+        export const KEY_5;
+        export const KEY_6;
+        export const KEY_7;
+        export const KEY_8;
+        export const KEY_9;
+        export const KEY_SEMICOLON;
+        export const KEY_EQUALS;
+        export const KEY_A;
+        export const KEY_B;
+        export const KEY_C;
+        export const KEY_D;
+        export const KEY_E;
+        export const KEY_F;
+        export const KEY_G;
+        export const KEY_H;
+        export const KEY_I;
+        export const KEY_J;
+        export const KEY_K;
+        export const KEY_L;
+        export const KEY_M;
+        export const KEY_N;
+        export const KEY_O;
+        export const KEY_P;
+        export const KEY_Q;
+        export const KEY_R;
+        export const KEY_S;
+        export const KEY_T;
+        export const KEY_U;
+        export const KEY_V;
+        export const KEY_W;
+        export const KEY_X;
+        export const KEY_Y;
+        export const KEY_Z;
+        export const KEY_CONTEXT_MENU;
+        export const KEY_NUMPAD0;
+        export const KEY_NUMPAD1;
+        export const KEY_NUMPAD2;
+        export const KEY_NUMPAD3;
+        export const KEY_NUMPAD4;
+        export const KEY_NUMPAD5;
+        export const KEY_NUMPAD6;
+        export const KEY_NUMPAD7;
+        export const KEY_NUMPAD8;
+        export const KEY_NUMPAD9;
+        export const KEY_MULTIPLY;
+        export const KEY_ADD;
+        export const KEY_SEPARATOR;
+        export const KEY_SUBTRACT;
+        export const KEY_DECIMAL;
+        export const KEY_DIVIDE;
+        export const KEY_F1;
+        export const KEY_F2;
+        export const KEY_F3;
+        export const KEY_F4;
+        export const KEY_F5;
+        export const KEY_F6;
+        export const KEY_F7;
+        export const KEY_F8;
+        export const KEY_F9;
+        export const KEY_F10;
+        export const KEY_F11;
+        export const KEY_F12;
+        export const KEY_F13;
+        export const KEY_F14;
+        export const KEY_F15;
+        export const KEY_F16;
+        export const KEY_F17;
+        export const KEY_F18;
+        export const KEY_F19;
+        export const KEY_F20;
+        export const KEY_F21;
+        export const KEY_F22;
+        export const KEY_F23;
+        export const KEY_F24;
+        export const KEY_NUM_LOCK;
+        export const KEY_SCROLL_LOCK;
+        export const KEY_COMMA;
+        export const KEY_PERIOD;
+        export const KEY_SLASH;
+        export const KEY_BACK_QUOTE;
+        export const KEY_OPEN_BRACKET;
+        export const KEY_BACK_SLASH;
+        export const KEY_CLOSE_BRACKET;
+        export const KEY_QUOTE;
+        export const KEY_META;
     }
 
     export default KeyCode;
