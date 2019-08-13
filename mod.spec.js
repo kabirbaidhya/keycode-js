bd023568bd131cbecf7294a84cfb16f490ebf1f0
Saugat Acharya <mesaugat@gmail.com>
2019-08-13 10:17:48 +0545

    Move spec to root directory and update build paths

--- mod.spec.js
+++ mod.spec.js
@@ -1,10 +1,11 @@
 const assert = require('chai').assert;
-const KeyCodeLib = require('../index');
-const KeyCodeDist = require('../dist/keycode.min.js');
+
+const KeyCodeLib = require('./dist/keycode.cjs.js');
+const KeyCodeDist = require('./dist/keycode.min.js');
 
 // Run tests.
 test(KeyCodeLib, 'KeyCode library (Common JS / EcmaScript imported module)');
-test(KeyCodeDist, 'KeyCode generated build (Minified dist file)');
+test(KeyCodeDist, 'KeyCode minified build');
 
 /**
  * Tests for KeyCode constants.
