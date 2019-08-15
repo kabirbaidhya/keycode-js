27c60f1b098def74375e9d197279244e8e86b8e1
Kabir Baidhya <kabirbaidhya@gmail.com>
2019-08-15 22:57:52 +0545

    Update test output text

--- mod.test.js
+++ mod.test.js
@@ -3,8 +3,8 @@ const { assert } = require('chai');
 const { defineTests } = require('./dist/test-definitions.cjs');
 const KeyCode = require('./dist/keycode.cjs.js');
 
-// Tests for Node (Common JS) Build.
-describe('Node (Common JS) Build', () => {
+// Tests for Node (Common JS) Module.
+describe('Node (Common JS) Module', () => {
   defineTests(KeyCode, {
     test: it,
     assertEquals: assert.equal
--- test.ts
+++ test.ts
@@ -4,7 +4,8 @@ import { assertEquals } from 'https://deno.land/std/testing/asserts.ts';
 import * as KeyCode from './mod.ts';
 import { defineTests } from './test-definitions.ts';
 
-// Tests for Deno.
+// Tests for Deno Module.
+console.log('Deno Module');
 defineTests(KeyCode, { test, assertEquals });
 
 runTests();
