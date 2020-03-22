aa37b6122bab84c8313dfd8fa9ec8b7075e650ee
Kabir Baidhya <kabirbaidhya@gmail.com>
2020-03-22 18:02:41 +0545

    Use std@v0.36.0/testing for deno

--- test/deno.test.ts
+++ test/deno.test.ts
@@ -1,8 +1,8 @@
-import { test, runTests } from 'https://deno.land/std/testing/mod.ts';
-import { assertEquals } from 'https://deno.land/std/testing/asserts.ts';
+import { assertEquals } from 'https://deno.land/std@v0.36.0/testing/asserts.ts';
 
 import * as mod from '../mod.ts';
 import { beginTests } from './tests.ts';
 
+const test = (name: string, fn: () => void) => Deno.test({ name, fn });
+
 beginTests(mod, { test, assertEquals });
-runTests();
